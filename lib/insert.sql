281ac3de5990b37e98d98d195b38170b4fcb39bc
mbugua <mbuguafaimba@gmail.com>
2023-02-15 22:21:38 +0300

    completed assignmrent

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr Chocolate', 7, 'male', 'brown', 'happy', TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 8, 'female', 'black', 'sad', TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 4, 'male', 'green', 'curious', TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 9, 'male', 'red', '', TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 3, 'female', 'yellow', 'bored', TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 2, 'male', 'white', 'sleepy', TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 7, 'female', 'rainbow', 'hungry', TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 8, 'male', 'silver', 'upset', TRUE);
